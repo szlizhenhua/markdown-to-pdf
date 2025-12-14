548d7cf6dfa52ae77dd4908543f4c0eeab09390c
szlizhenhua <143886703+szlizhenhua@users.noreply.github.com>
2025-12-15 00:15:55 +0800

    fix: 将广告脚本从组件形式更改为普通脚本标签

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,13 +47,7 @@ export default function RootLayout({
           strategy="afterInteractive"
         />
         {/* 广告脚本 */}
-        <Script
-          src="https://quge5.com/88/tag.min.js"
-          data-zone="193056"
-          data-cfasync="false"
-          async
-          strategy="afterInteractive"
-        />
+        <script src="https://quge5.com/88/tag.min.js" data-zone="193056" async data-cfasync="false"></script>
       </head>
       <body
         className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}
