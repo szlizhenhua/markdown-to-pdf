be4ff160d28ef6a02e449c94caf007759155c0fa
szlizhenhua <143886703+szlizhenhua@users.noreply.github.com>
2025-12-15 00:21:44 +0800

    fix: 更新服务工作者的zoneId为10324648

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10324528
+    "zoneId": 10324648
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
